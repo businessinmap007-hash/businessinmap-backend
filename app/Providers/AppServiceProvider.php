985757dc1cdd4c5407bf3d9abfa82f2e29ca0d53
businessinmap007-hash <businessinmap007@gmail.com>
2026-06-27 05:39:09 +0300

    Remove separate guarantee routes loader

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use App\Models\Deposit;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
@@ -26,7 +25,6 @@ class AppServiceProvider extends ServiceProvider
         $this->configureLocale();
         $this->configureUrl();
         $this->shareAdminV2Data();
-        $this->loadAdminV2GuaranteeRoutes();
     }
 
     private function configureLocale(): void
@@ -78,14 +76,5 @@ class AppServiceProvider extends ServiceProvider
 
             $view->with('openDisputesCount', $openDisputesCount);
         });
-    }
-
-    private function loadAdminV2GuaranteeRoutes(): void
-    {
-        $path = base_path('routes/admin_v2_guarantees.php');
-
-        if (file_exists($path)) {
-            Route::group([], $path);
-        }
     }
 }

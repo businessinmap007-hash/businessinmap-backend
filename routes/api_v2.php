cfd5132993e98f2ffe75bb8da12b43c9237b070c
businessinmap007-hash <businessinmap007@gmail.com>
2026-06-28 04:17:05 +0300

    Add API v2 offer comparison routes

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V2\GuaranteeController;
+use App\Http\Controllers\Api\V2\OfferComparisonController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v2')->group(function () {
@@ -11,6 +12,11 @@ Route::prefix('v2')->group(function () {
             Route::get('transactions', [GuaranteeController::class, 'transactions']);
             Route::post('activate', [GuaranteeController::class, 'activate']);
             Route::post('check-operation', [GuaranteeController::class, 'checkOperationCoverage']);
+        });
+
+        Route::prefix('offers')->group(function () {
+            Route::get('compare', [OfferComparisonController::class, 'compare']);
+            Route::post('compare', [OfferComparisonController::class, 'compare']);
         });
     });
 });

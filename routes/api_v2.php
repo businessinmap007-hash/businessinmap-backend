046be2241000065b8a53b247a643dc6a75a38b99
businessinmap007-hash <businessinmap007@gmail.com>
2026-06-28 03:06:17 +0300

    Add API v2 guarantee routes file

---
+++ routes/api_v2.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\V2\GuaranteeController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v2')->group(function () {
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::prefix('guarantees')->group(function () {
+            Route::get('levels', [GuaranteeController::class, 'levels']);
+            Route::get('me', [GuaranteeController::class, 'me']);
+            Route::get('transactions', [GuaranteeController::class, 'transactions']);
+            Route::post('activate', [GuaranteeController::class, 'activate']);
+        });
+    });
+});

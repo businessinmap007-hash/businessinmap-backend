347a39e82164159e4f801f12caf27a4845354618
businessinmap007-hash <businessinmap007@gmail.com>
2026-06-28 04:03:04 +0300

    Add bookable allocations create view

---
+++ resources/views/admin-v2/bookable-allocations/create.blade.php
@@ -0,0 +1,29 @@
+@extends('admin-v2.layouts.master')
+
+@section('title', 'Create Bookable Allocation')
+@section('topbar_title', 'Create Bookable Allocation')
+@section('body_class', 'admin-v2-bookable-allocations')
+
+@section('content')
+<div class="a2-page">
+    <div class="a2-page-head">
+        <div>
+            <h1 class="a2-page-title">إنشاء حصة وحدة</h1>
+            <div class="a2-page-subtitle">تحديد حصة لشريك من غرفة أو وحدة قابلة للحجز مع توليد عرض قابل للمقارنة.</div>
+        </div>
+        <div class="a2-page-actions">
+            <a href="{{ route('admin.bookable-allocations.index') }}" class="a2-btn a2-btn-ghost">رجوع</a>
+        </div>
+    </div>
+
+    <form method="POST" action="{{ route('admin.bookable-allocations.store') }}">
+        @csrf
+        @include('admin-v2.bookable-allocations._form', [
+            'allocation' => $allocation,
+            'partnerships' => $partnerships,
+            'bookables' => $bookables,
+            'services' => $services,
+        ])
+    </form>
+</div>
+@endsection
